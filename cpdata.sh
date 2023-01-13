b708a71e03bfbe5d2d76b1fb6dbd7a9ec62797df
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-01-13 11:31:58 -0500

    230113

---
+++ cpdata.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+: 'module for copying data from source directory to destination directory'
+
+#define directories
+loc=`pwd`
+dest=$loc/data_for_copy; mkdir -p $dest
+echo "destination directory is ${dest}"
+
+#parameter input
+FILES=( T1 RS1 )     #define files types to copy
+ext=nii      #define file extension
+
+SUB=`cat id_sub`
+for sub in ${SUB[@]}
+do
+  mkdir -p ${dest}/${sub}
+  for file in ${FILES[@]}
+  do
+    if [ ! -f ${dest}/${sub}/${sub}.${file}.${ext} ]; then
+      echo "\ncopying ${sub} ${file} to destination directory"
+      cp ${sub}/${sub}.${file}.${ext} ${dest}/${sub}/
+    else
+      echo "${sub} ${file} already copied to destination directory  \nskipping condition"
+    fi
+  done
+done
