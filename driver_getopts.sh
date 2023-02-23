3bdf9ac47f623dd9fcd6215f0327f36642824cf6
jessedesimone <52834672+jessedesimone@users.noreply.github.com>
2023-02-23 13:26:53 -0500

    230223

--- driver_getopts.sh
+++ driver_getopts.sh
@@ -3,17 +3,18 @@
 
 gen_error_msg="\
 
-    Usage: ./driver.sh [-a] [-b] [-c] | [-h]
+    Usage: ./driver.sh [-a] [-b] [-c] [-o <path/to/outdir>| [-h]
     Required arguments (at least 1 of):
     -a      <option description>
     -b      <option description>
     -c      <option description>
+    -d      specify output directory
 
     Option arguments:
     -h      help
 
     "
-    while getopts ":habc" opt; do
+    while getopts ":habco:" opt; do
         case ${opt} in
                 h|\?) #help option
                     echo "$gen_error_msg"
@@ -28,13 +29,21 @@ gen_error_msg="\
                 c) #option c
                     cflag=1
                     ;;
+                o) #output
+                    oflag=${OPTARG}
+                    ;;
         esac
     done
     if [ $OPTIND -eq 1 ]; then
-        echo "++ driver.sh requires argument"
+        echo "++ driver.sh requires at least 1 argument argument"
         echo "$gen_error_msg"
         exit 1
-        fi
+    fi
+    if [ ! "$oflag" ]; then 
+        echo "ERROR: [-o <string>] must be provided"
+        echo "$gen_error_msg" >&2
+        exit 1
+    fi
     shift $((OPTIND -1))
 
 #=======handle options========
